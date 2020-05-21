4b080d2e50fcc4b493f158a7491806d911c02737
40grivenprog <grivenmus40@yandex.ru>
2020-05-21 13:01:41 +0300

    Fix comments

---
+++ db/migrate/20200519214923_tests_and_users.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class TestsAndUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tests_users do |t|
+      t.references :user, foreign_key: true
+      t.references :test, foreign_key: true
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+# frozen_string_literal: true
+
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
 #
+#      insert into tests_users (test_id, user_id)
+#     Values
+#     (2,1),
+#     (1,1),
+#     (3,1),
+#     (4,2)
+#     ;
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.create(title: 'WEB')
+Category.create(title: 'SQL')
+Category.create(title: 'Programming')
+Test.create(title: 'Ruby', category_id: 3)
+Test.create(title: 'Rails', category_id: 3)
+Test.create(title: 'Postgres', category_id: 2)
+Test.create(title: 'HTTP', category_id: 1)
+Question.create(body: 'What is HTTP?', test_id: 4)
+Question.create(body: 'What is Migration?', test_id: 2)
+Question.create(body: 'What is Rails Model?', test_id: 2)
+Question.create(body: 'What is Instance Methods?', test_id: 1)
+Question.create(body: 'How to describe table in Postgres?', test_id: 3)
+Question.create(body: 'What is Class Methods', test_id: 1)
+Answer.create(body: 'Methods of Class Object', question_id: 4)
+Answer.create(body: 'Methods of the current Class', question_id: 6)
+User.create(email: 'maksim@yandex.ru')
+User.create(email: 'andrey@mail.ru')
