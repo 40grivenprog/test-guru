944986d55d65d9dc1af8f2eded44486d1099c0e7
40grivenprog <grivenmus40@yandex.ru>
2020-05-24 18:03:38 +0300

    Fix question.rb

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,11 +5,5 @@ class Question < ApplicationRecord
   has_many :answers
   validates :body, presence: true
   validate :valid_answers_number?
-
-  private
-
-  def valid_answers_number?
-    number_of_answers = answers.length
-    errors.add(:answers) if number_of_answers < 1 || number_of_answers > 4
-  end
+  validates_length_of :answers, minimum: 1, maximum: 4
 end
