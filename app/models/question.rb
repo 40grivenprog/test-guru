87d6ac0beda97522a2697212536b907b3a0c8826
40grivenprog <grivenmus40@yandex.ru>
2020-05-24 16:47:25 +0300

    lesson6

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,13 @@
 class Question < ApplicationRecord
   belongs_to :test
   has_many :answers
+  validates :body, presence: true
+  validate :valid_answers_number?
+
+  private
+
+  def valid_answers_number?
+    number_of_answers = answers.length
+    errors.add(:answers) if number_of_answers < 1 || number_of_answers > 4
+  end
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -6,7 +6,18 @@ class Test < ApplicationRecord
   has_many :tests_users
   has_many :users, through: :tests_users
   belongs_to :creator, class_name: 'User'
-  def self.test_by_categories(category_name)
-    Test.joins('JOIN categories on tests.category_id = categories.id').where('categories.title = ?', category_name).order(title: :desc).pluck(:title)
+  scope :easy, -> { where(level: 0..1) }
+  scope :medium, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
+  scope :test_by_category, ->(category_name) { joins('JOIN categories on tests.category_id = categories.id').where('categories.title = ?', category_name).order(title: :desc).pluck(:title) }
+  validates :title, presence: true
+  validates :level, numericality: { greater_than_or_equal_to: 0 }
+  validate :uniq_test?
+
+  private
+
+  def uniq_test?
+    not_uniq_tests = Test.where('title = ? and level = ?', title, level)
+    errors.messages[:test] << 'not uniq!' unless not_uniq_tests.empty?
   end
 end
