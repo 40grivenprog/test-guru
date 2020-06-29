7720f75676c785b81aa15369cd462d2f9ccc1c6b
40grivenprog <grivenmus40@yandex.ru>
2020-06-29 20:51:59 +0300

    Add restart possibility to Tests

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -1,23 +1,30 @@
 # frozen_string_literal: true
 
 class TestPassagesController < ApplicationController
-  before_action :set_test_passage, only: %i[show result update]
+  before_action :set_test_passage, only: %i[show create result update restart]
   before_action :authenticate_user!
 
-  def show; end
+  def show
+    if @test_passage.complete?
+      redirect_to result_test_passage_path(@test_passage)
+    end
+  end
 
   def result; end
 
   def update
     @test_passage.accept!(params[:answer_ids])
-
     if @test_passage.complete?
-      TestsMailer.completed_test(@test_passage).deliver_now
+      #TestsMailer.completed_test(@test_passage).deliver_now
       redirect_to result_test_passage_path(@test_passage)
     else
-      @test_passage.touch
       render :show
     end
+  end
+
+  def restart
+    @test_passage.restart
+    redirect_to test_passage_path @test_passage
   end
 
   private
