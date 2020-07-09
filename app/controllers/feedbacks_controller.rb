11abfbe30e5f7da531fb399deadf7d4ff4d76006
40grivenprog <grivenmus40@yandex.ru>
2020-07-09 14:04:44 +0300

    Fix notes

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,21 +1,21 @@
 class FeedbacksController < ApplicationController
-	def new
-	end
+  def new
+  end
 
-	def create
-		if feedback_params[:subject].present? && feedback_params[:body].present?
-			FeedbacksMailer.feedback(feedback_params, current_user).deliver_now
-			redirect_to root_path
-		else
-			flash['alert'] = t('.feedback_error')
-			render 'new'
-		end
-	end
+  def create
+    if feedback_params[:subject].present? && feedback_params[:body].present?
+      FeedbacksMailer.feedback(feedback_params, current_user).deliver_now
+      redirect_to root_path
+    else
+      flash['alert'] = t('.feedback_error')
+      render 'new'
+    end
+  end
 
-	private
+  private
 
-	def feedback_params
-		params.permit(:subject, :body)
-	end
+  def feedback_params
+    params.permit(:subject, :body)
+  end
 
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -3,7 +3,7 @@
 class Test < ApplicationRecord
   belongs_to :category
   has_many :questions
-  has_many :test_passages
+  has_many :test_passages, dependent: :destroy
   has_many :users, through: :test_passages
   belongs_to :creator, class_name: 'User'
 
