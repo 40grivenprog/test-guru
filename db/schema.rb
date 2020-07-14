68910e862ac5168cec2a7af36bac7d596a5619d4
40grivenprog <grivenmus40@yandex.ru>
2020-07-14 12:44:14 +0300

    Change migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_13_140157) do
+ActiveRecord::Schema.define(version: 2020_07_10_120053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,7 +53,6 @@ ActiveRecord::Schema.define(version: 2020_07_13_140157) do
     t.integer "test_id"
     t.integer "correct_questions", default: 0
     t.integer "current_question_id"
-    t.integer "attemp"
     t.index ["current_question_id"], name: "index_test_passages_on_current_question_id"
     t.index ["test_id"], name: "index_test_passages_on_test_id"
     t.index ["user_id"], name: "index_test_passages_on_user_id"
